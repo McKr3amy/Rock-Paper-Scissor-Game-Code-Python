b1b07bc1bbb00c695fd5d1b5a387dac02dfb9eb0
Artivain <thomas@artivain.tk>
2021-08-16 18:33:48 -0400

    Possibilité de rejouer Améliorations globales (ex: avertissement quand la réponse est pas correct)

--- main.py
+++ main.py
@@ -1,41 +1,64 @@
 import random
 import time
 mylist = ["Roche", "Papier", "Ciseaux"]
-y = (random.choice(mylist))
-x = input("Roche, Papier ou Ciseaux? ")
-if x == "Roche" and y == "Papier":
-    print("J'ai " + y)
-    time.sleep(0.5)
-    print("J'ai Ganger")
-if x == "Roche" and y == "Roche":
-    print("J'ai " + y)
-    time.sleep(0.5)
-    print("Égalité")
-if x == "Roche" and y == "Ciseaux":
-    print("J'ai " + y)
-    time.sleep(0.5)
-    print("J'ai Perdu")
-if x == "Papier" and y == "Papier":
-    print("J'ai " + y)
-    time.sleep(0.5)
-    print("Égalité")
-if x == "Papier" and y == "Roche":
-    print("J'ai " + y)
-    time.sleep(0.5)
-    print("J'ai Gagné")
-if x == "Papier" and y == "Ciseaux":
-    print("J'ai " + y)
-    time.sleep(0.5)
-    print("J'ai Perdu")
-if x == "Ciseaux" and y == "Papier":
-    print("J'ai " + y)
-    time.sleep(0.5)
-    print("J'ai Gagné")
-if x == "Ciseaux" and y == "Roche":
-    print("J'ai " + y)
-    time.sleep(0.5)
-    print("J'ai Perdu")
-if x == "Ciseaux" and y == "Ciseaux":
-    print("J'ai " + y)
-    time.sleep(0.5)
-    print("Égalité")
+def play():
+	y = (random.choice(mylist))
+	x = input("Roche, Papier ou Ciseaux? ")
+	if x != "Roche" and x != "Papier" and x != "Ciseaux":
+		print("Réponse invalide.")
+		return play()
+	if x == "Roche" and y == "Papier":
+			print("J'ai " + y)
+			time.sleep(0.5)
+			print("J'ai Ganger")
+	if x == "Roche" and y == "Roche":
+			print("J'ai " + y)
+			time.sleep(0.5)
+			print("Égalité")
+	if x == "Roche" and y == "Ciseaux":
+			print("J'ai " + y)
+			time.sleep(0.5)
+			print("J'ai Perdu")
+	if x == "Papier" and y == "Papier":
+			print("J'ai " + y)
+			time.sleep(0.5)
+			print("Égalité")
+	if x == "Papier" and y == "Roche":
+			print("J'ai " + y)
+			time.sleep(0.5)
+			print("J'ai Gagné")
+	if x == "Papier" and y == "Ciseaux":
+			print("J'ai " + y)
+			time.sleep(0.5)
+			print("J'ai Perdu")
+	if x == "Ciseaux" and y == "Papier":
+			print("J'ai " + y)
+			time.sleep(0.5)
+			print("J'ai Gagné")
+	if x == "Ciseaux" and y == "Roche":
+			print("J'ai " + y)
+			time.sleep(0.5)
+			print("J'ai Perdu")
+	if x == "Ciseaux" and y == "Ciseaux":
+			print("J'ai " + y)
+			time.sleep(0.5)
+			print("Égalité")
+
+def continuer():
+	q = input("Continuer de jouer? (Oui | Non) [Oui]: ")
+	if q == 'Oui' or q == "":
+		return True
+	elif q == 'Non':
+		return False
+	else :
+		print("Réponse invalide.")
+		return continuer()
+
+play()
+
+while continuer():
+	play()
+
+print("Bye!")
+time.sleep(2)
+raise SystemExit
